4044c283f6f4b3261c7f943a0d62197396d178ba
Gaidukov Taras <kemaweyan@gmail.com>
2017-03-04 18:04:48 +0200

    Created model Image in the 'gallery' application

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class Image(models.Model):
     src = models.ImageField()
     position = models.IntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
