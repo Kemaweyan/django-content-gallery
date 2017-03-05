cdeff95ba83e4ec6f7e33d58271a80a4566cdda4
Gaidukov Taras <kemaweyan@gmail.com>
2017-03-05 23:05:36 +0200

    Added image deletion when image object is being deleted

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 from slugify import UniqueSlugify
 
@@ -26,3 +28,8 @@ class Image(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+
+
+@receiver(pre_delete, sender=Image)
+def mymodel_delete(sender, instance, **kwargs):
+    instance.src.delete(False)
