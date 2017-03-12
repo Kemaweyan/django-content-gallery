641db5fe642d6cace67a370c4acf3cdda440037b
Gaidukov Taras <kemaweyan@gmail.com>
2017-03-12 19:31:10 +0200

    Moved helper functions from gallery.models to gallery.utils

--- models.py
+++ models.py
@@ -7,29 +7,13 @@ from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
 
 from slugify import UniqueSlugify
-from PIL import Image as Img
 
-from . import settings
 from . import utils
-
-IMG_W = settings.GALLERY_IMAGE_WIDTH
-IMG_H = settings.GALLERY_IMAGE_HEIGHT
-
-THUMB_W = settings.GALLERY_THUMBNAIL_WIDTH
-THUMB_H = settings.GALLERY_THUMBNAIL_HEIGHT
-
-def _make_src(slug, filename=''):
-    if filename and '.' in filename:
-        ext = filename.split('.')[-1]
-        src = "{}.{}".format(slug, ext)
-    else:
-        src = slug
-    return os.path.join('gallery', src)
 
 def _unique_src_check(slug, uids):
     if slug in uids:
         return False
-    src = _make_src(slug)
+    src = utils.make_src(slug)
     return not Image.objects.filter(src__startswith=src)
 
 slugify_unique = UniqueSlugify(unique_check=_unique_src_check, to_lower=True)
@@ -37,24 +21,8 @@ slugify_unique = UniqueSlugify(unique_check=_unique_src_check, to_lower=True)
 def _upload_rename(instance, filename):
     title = str(instance.content_object)
     slug = slugify_unique(title)
-    src = _make_src(slug, filename)
+    src = utils.make_src(slug, filename)
     return src
-
-def _resize(path, size, rename=""):
-    img = Img.open(path)
-    img.thumbnail(size)
-    if rename:
-        path = rename
-    img.save(path, img.format)    
-
-def _resize_image(path):
-    size = (IMG_W, IMG_H)
-    _resize(path, size)
-
-def _create_thumbnail(path):
-    size = (THUMB_W, THUMB_H)
-    thumb_path = utils.create_thumbnail_path(path)
-    _resize(path, size, thumb_path)
    
 
 class Image(models.Model):
@@ -76,8 +44,8 @@ class Image(models.Model):
             if images:
                 self.position = images[0].position + 1
         super().save(*args, **kwargs)
-        _resize_image(self.src.path)
-        _create_thumbnail(self.src.path)
+        utils.resize_image(self.src.path)
+        utils.create_thumbnail(self.src.path)
 
 
 @receiver(pre_delete, sender=Image)
--- utils.py
+++ utils.py
@@ -1,7 +1,24 @@
 import re
 import os
+from PIL import Image
 
 from django.core import urlresolvers as ur
+
+from . import settings
+
+IMG_W = settings.GALLERY_IMAGE_WIDTH
+IMG_H = settings.GALLERY_IMAGE_HEIGHT
+
+THUMB_W = settings.GALLERY_THUMBNAIL_WIDTH
+THUMB_H = settings.GALLERY_THUMBNAIL_HEIGHT
+
+def make_src(slug, filename=''):
+    if filename and '.' in filename:
+        name, ext = os.path.splitext(filename)
+        src = "{}{}".format(slug, ext)
+    else:
+        src = slug
+    return os.path.join('gallery', src)
 
 def get_choices_url_pattern():
     choices_url = ur.reverse('gallery:choices', args=(0,))
@@ -10,3 +27,19 @@ def get_choices_url_pattern():
 def create_thumbnail_path(path):
     name, ext = os.path.splitext(path)
     return "{}_thumbnail{}".format(name, ext)
+
+def resize(path, size, rename=""):
+    img = Image.open(path)
+    img.thumbnail(size)
+    if rename:
+        path = rename
+    img.save(path, img.format)    
+
+def resize_image(path):
+    size = (IMG_W, IMG_H)
+    resize(path, size)
+
+def create_thumbnail(path):
+    size = (THUMB_W, THUMB_H)
+    thumb_path = create_thumbnail_path(path)
+    resize(path, size, thumb_path)
