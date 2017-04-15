74ac2895886d8d95c04921c176aa7d24c48bc4e6
Gaidukov Taras <kemaweyan@gmail.com>
2017-04-15 22:49:56 +0300

    Changed ImageAdminInline superclass to GenericInlineModelAdmin to use custon template

--- admin.py
+++ admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericStackedInline
+from django.contrib.contenttypes.admin import GenericInlineModelAdmin
 
 from . import models
 from . import forms
 
-class ImageAdminInline(GenericStackedInline):
+class ImageAdminInline(GenericInlineModelAdmin):
     model = models.Image
     form = forms.ImageAdminInlineForm
+    template = "gallery/edit_inline/image_admin.html"
 
 class ImageAdmin(admin.ModelAdmin):
     form = forms.ImageAdminForm
