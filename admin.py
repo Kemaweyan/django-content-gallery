32cbf8f6b993a70ce65fd52e29e1d81d93c62c1b
Gaidukov Taras <kemaweyan@gmail.com>
2017-04-15 17:05:51 +0300

    Added inline admin form

--- admin.py
+++ admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericStackedInline
 
 from . import models
 from . import forms
+
+class ImageAdminInline(GenericStackedInline):
+    model = models.Image
 
 class ImageAdmin(admin.ModelAdmin):
     form = forms.ImageAdminForm
