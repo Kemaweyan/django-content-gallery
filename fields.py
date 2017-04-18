172b6bb109c0ea1603d577040c372bd5e4e6920a
Gaidukov Taras <kemaweyan@gmail.com>
2017-04-18 17:23:50 +0300

    Moved ImageFile and InMemoryImageData classes into separate module

--- fields.py
+++ fields.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.fields import files
 
 from . import settings
-from . import utils
+from . import image_files
 
 # the size of image
 IMG_W = settings.GALLERY_IMAGE_WIDTH
@@ -28,14 +28,14 @@ class GalleryImageFieldFile(files.ImageFieldFile):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.thumbnail = utils.ImageFile(self, THUMB_W, THUMB_H,
+        self.thumbnail = image_files.ImageFile(self, THUMB_W, THUMB_H,
             'thumbnail')
-        self.image_data = utils.InMemoryImageData(self, IMG_W, IMG_H)
-        self.small_image = utils.ImageFile(self, SMALL_W, SMALL_H,
+        self.image_data = image_files.InMemoryImageData(self, IMG_W, IMG_H)
+        self.small_image = image_files.ImageFile(self, SMALL_W, SMALL_H,
             'small')
-        self.preview = utils.ImageFile(self, PREVIEW_W, PREVIEW_H,
+        self.preview = image_files.ImageFile(self, PREVIEW_W, PREVIEW_H,
             'preview')
-        self.small_preview = utils.ImageFile(self, SMALL_PREVIEW_W,
+        self.small_preview = image_files.ImageFile(self, SMALL_PREVIEW_W,
             SMALL_PREVIEW_H, 'small_preview')
 
     def save_files(self, slug, name):
--- utils.py
+++ utils.py
@@ -4,10 +4,9 @@ import re
 import io
 from PIL import Image
 import magic
-from abc import ABCMeta, abstractmethod
 
-from django.core import urlresolvers as ur
-from django.core.files import uploadedfile as uf
+from django.core import urlresolvers
+from django.core.files import uploadedfile
 
 from . import settings
 
@@ -17,7 +16,7 @@ def get_choices_url_pattern():
     via AJAX in JS code.
     """
     # get 'choices' URL with any id
-    choices_url = ur.reverse('gallery:choices', args=(0,))
+    choices_url = urlresolvers.reverse('gallery:choices', args=(0,))
     # remove id (last digits in the URL)
     return re.sub(r'\d+$', '', choices_url)
 
@@ -51,103 +50,14 @@ def create_url(filename):
 def name_in_db(name):
     return os.path.join(settings.GALLERY_PATH, name)
 
+def image_resize(src, dst, size):
+    img = Image.open(src)
+    img.thumbnail(size)
+    img.save(dst, img.format)
 
-class BaseImageData(metaclass=ABCMeta):
-
-    def __init__(self, image, width, height):
-        self._set_name(image.name)
-        self.size = (width, height)
-
-    def _set_name(self, name):
-        self.name = os.path.basename(name)
-
-    @property
-    def filename(self):
-        return self._create_filename(self.name)
-
-    @property
-    def path(self):
-        return create_path(self.filename)
-
-    @property
-    def url(self):
-        return create_url(self.filename)
-
-    def _resize(self, src, dst):
-        img = Image.open(src)
-        img.thumbnail(self.size)
-        img.save(dst, img.format)
-
-    @abstractmethod
-    def _create_image(self, image):
-        pass
-
-    @abstractmethod
-    def _create_filename(self, filename):
-        pass
-
-    def _change_ext(self, filename):
-        name = get_name(self.name)
-        ext = get_ext(filename)
-        self.name = name + ext
-
-    def save(self, image, slug, name):
-        is_uploaded = '/' not in image.name
-        self._set_name(name)
-        if is_uploaded and self.name:
-            self.delete()
-        if slug:
-            new_name = slug + get_ext(image.name)
-            if self.name and not is_uploaded:
-                self._rename_file(new_name)
-            self.name = new_name
-        if is_uploaded:
-            if not slug:
-                self._change_ext(image.name)
-            self._create_image(image)
-
-    def _rename_file(self, name):
-        new_filename = self._create_filename(name)
-        new_path = create_path(new_filename)
-        os.rename(self.path, new_path)
-
-    def delete(self):
-        try:
-            os.remove(self.path)
-        except FileNotFoundError:
-            pass
-
-
-class ImageFile(BaseImageData):
-
-    def __init__(self, image, width, height, word):
-        self.word = word
-        super().__init__(image, width, height)
-
-    def _create_filename(self, filename):
-        name, ext = os.path.splitext(filename)
-        return "{}_{}{}".format(name, self.word, ext)
-
-    def _create_image(self, image):
-        self._resize(image, self.path)
-
-
-class InMemoryImageData(BaseImageData):
-
-    def __init__(self, image, width, height):
-        super().__init__(image, width, height)
-        self.data = None
-
-    def _create_filename(self, filename):
-        return filename
-
-    def _create_image(self, image):
-        output = io.BytesIO()
-        self._resize(image, output)
-        mime = magic.from_buffer(output.getvalue(), mime=True)
-        self.data = uf.InMemoryUploadedFile(output, 'ImageField', self.name,
-            mime, sys.getsizeof(output), None)
-
-    @property
-    def name_in_db(self):
-        return name_in_db(self.name)
+def create_in_memory_image(image, name, size):
+    output = io.BytesIO()
+    image_resize(image, output, size)
+    mime = magic.from_buffer(output.getvalue(), mime=True)
+    return uploadedfile.InMemoryUploadedFile(output, 'ImageField', name,
+        mime, sys.getsizeof(output), None)
