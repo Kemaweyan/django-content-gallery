3d715d297a86f37a1eba10e8b8aad4320af0efaf
Gaidukov Taras <kemaweyan@gmail.com>
2017-04-22 21:13:38 +0300

    Changed inline_admin template

--- admin.py
+++ admin.py
@@ -8,12 +8,15 @@ class ImageAdminInline(GenericInlineModelAdmin):
     model = models.Image
     form = forms.ImageAdminInlineForm
     template = "gallery/edit_inline/image_admin.html"
-    extra = 1
+    extra = 0
 
     class Media:
         js = (
             "https://code.jquery.com/ui/1.12.1/jquery-ui.js",
         )
+        css = {
+            "all": ("/static/admin/css/inline_admin.css",)
+        }
 
 class ImageAdmin(admin.ModelAdmin):
     form = forms.ImageAdminForm
