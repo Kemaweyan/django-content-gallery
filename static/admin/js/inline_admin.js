aa374c2e4a4a5fe9b66fd895aa0a58cf3bb11f43
Gaidukov Taras <kemaweyan@gmail.com>
2017-04-24 20:53:22 +0300

    Added new image displaying in admin inline form

--- static/admin/js/inline_admin.js
+++ static/admin/js/inline_admin.js
@@ -19,7 +19,74 @@
         });
 
         $("#id_image").on("change", function () {
-            
+            var image_id = this.value;
+
+            var $group = $(".gallery-inline-formset");
+
+            var opts = $group.attr("data-inline-formset");
+            var obj = JSON.parse(opts);
+            var prefix = obj.options.prefix;
+
+            var $totla_forms = $group.find("#id_" + prefix + "-TOTAL_FORMS");
+            var new_id = $totla_forms.val();
+
+            var new_prefix = prefix + "-" + new_id;
+            $totla_forms.val(parseInt(new_id) + 1);
+
+            var url = "/admin/gallery/image/ajax/preview/" + image_id;
+            $.ajax({
+                url: url,
+                dataType: "json",
+                beforeSend: function(xhr) {
+                    if (xhr.overrideMimeType)
+                        xhr.overrideMimeType("application/json");
+                },
+                success: function (response) {
+                    $("#sortedImages").find(".addNewImage").before(
+                        $("<div></div>")
+                        .addClass("imageObject")
+                        .append(
+                            $("<div></div>")
+                            .addClass("imageHeader")
+                            .addClass("imageHeaderBg")
+                        )
+                        .append(
+                            $("<div></div>")
+                            .addClass("imageContent")
+                            .append(
+                                $("<span></span>")
+                                .addClass("image-delete")
+                                .append(
+                                    $("<input>")
+                                    .attr("type", "hidden")
+                                    .attr("id", "id_" + new_prefix + "-DELETE")
+                                    .attr("name", new_prefix + "-DELETE")
+                                    .val(0)
+                                )
+                            )
+                            .append(
+                                $("<img>")
+                                .attr("src", response.preview_url)
+                            )
+                                .append(
+                                $("<input>")
+                                .addClass("image-position")
+                                .attr("type", "hidden")
+                                .attr("id", "id_" + new_prefix + "-position")
+                                .attr("name", new_prefix + "-position")
+                                .val(response.position)
+                            )
+                            .append(
+                                $("<input>")
+                                .attr("type", "hidden")
+                                .attr("id", "id_" + new_prefix + "-id")
+                                .attr("name", new_prefix + "-id")
+                                .val(image_id)
+                            )
+                        )
+                    );
+                }
+            });
         });
     });
 })(django.jQuery);
