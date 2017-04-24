9293dc3e70f7eb5d12b5605e9024f47b63dca4f5
Gaidukov Taras <kemaweyan@gmail.com>
2017-04-24 17:46:23 +0300

    Added ajax view for new image preview in admin

--- admin.py
+++ admin.py
@@ -1,5 +1,10 @@
+import json
+
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericInlineModelAdmin
+from django.conf.urls import url
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 
 from . import models
 from . import forms
@@ -23,7 +28,27 @@ class ImageAdminInline(GenericInlineModelAdmin):
         qs = super().get_queryset(request)
         return qs.order_by('position')
 
+
 class ImageAdmin(admin.ModelAdmin):
     form = forms.ImageAdminForm
+
+    def get_urls(self):
+        urls = super().get_urls()
+        extra_urls = [
+            url(
+                r'^ajax/preview/(?P<pk>\d+)/$',
+                self.preview,
+                name='gallery_new_image_preview'
+            ),
+        ]
+        return extra_urls + urls
+
+    def preview(self, request, pk):
+        image = get_object_or_404(models.Image, pk=pk)
+        response = {
+            "preview_url": image.preview_url,
+            "position": image.position,
+        }
+        return HttpResponse(json.dumps(response), content_type='application/json')
 
 admin.site.register(models.Image, ImageAdmin)
