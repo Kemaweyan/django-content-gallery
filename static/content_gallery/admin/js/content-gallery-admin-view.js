08af82538c416e29626fc93057bf8e1841369ab0
Gaidukov Taras <kemaweyan@gmail.com>
2017-05-05 19:10:00 +0300

    Changed css class name of images container

--- static/content_gallery/admin/js/content-gallery-admin-view.js
+++ static/content_gallery/admin/js/content-gallery-admin-view.js
@@ -55,7 +55,7 @@
 
         var $adminImageView = $("#content-gallery-admin-preview");
 
-        $(".content-gallery-inline-formset").on("click", ".content-gallery-image-preview", function (e) {
+        $(".content-gallery-images").on("click", ".content-gallery-image-preview", function (e) {
             e.preventDefault();
 
             var data = JSON.parse($(this).attr("data-image"));
