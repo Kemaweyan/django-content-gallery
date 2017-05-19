4f3609cad2ff726259d71c5af9a496d5b3a292f7
Gaidukov Taras <kemaweyan@gmail.com>
2017-05-19 17:19:31 +0300

    Moved ImageTestCase to tests.models

--- tests/models.py
+++ tests/models.py
@@ -1,14 +1,55 @@
-from django.db import models
+from django.db import models as django_models
+from django.test import TestCase, mock
+from django.contrib.contenttypes.models import ContentType
 
-class TestModel(models.Model):
-    name = models.CharField(max_length=100)
+from .. import models
+from .. import settings
+
+from .utils import get_image_in_memory_data
+
+class TestModel(django_models.Model):
+    name = django_models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class AnotherTestModel(models.Model):
-    name = models.CharField(max_length=100)
+class AnotherTestModel(django_models.Model):
+    name = django_models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+
+class ImageTestCase(TestCase):
+
+    @mock.patch('gallery.models.slugify_unique', return_value='foo')
+    def setUp(self, slugify_unique):
+        self.object = TestModel(name="TestObject")
+        self.object.save()
+        self.second_object = TestModel(name="SecondTestObject")
+        self.second_object.save()
+        self.another_object = AnotherTestModel(name="AnotherTestObject")
+        self.another_object.save()
+        self.image = models.Image(
+            image=get_image_in_memory_data(),
+            position=0,
+            content_type=ContentType.objects.get_for_model(TestModel),
+            object_id=self.object.id
+        )
+        self.image.save()
+        self.image = self.get_image()
+
+    def tearDown(self):
+        self.image.delete()
+        self.object.delete()
+        self.second_object.delete()
+        self.another_object.delete()
+
+    @staticmethod
+    def get_image():
+        return models.Image.objects.all()[0]
+
+    @staticmethod
+    def get_name(name):
+        return "/".join([settings.GALLERY_PATH, name])
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,46 +1,10 @@
-import tempfile, shutil
-
-from django.test import TestCase, mock
+from django.test import mock
 from django.contrib.contenttypes.models import ContentType
 
 from .. import models
-from .. import settings
 
-from .models import TestModel, AnotherTestModel
+from .models import TestModel, AnotherTestModel, ImageTestCase
 from .utils import get_image_in_memory_data
-
-class ImageTestCase(TestCase):
-
-    @mock.patch('gallery.models.slugify_unique', return_value='foo')
-    def setUp(self, slugify_unique):
-        self.object = TestModel(name="TestObject")
-        self.object.save()
-        self.second_object = TestModel(name="SecondTestObject")
-        self.second_object.save()
-        self.another_object = AnotherTestModel(name="AnotherTestObject")
-        self.another_object.save()
-        self.image = models.Image(
-            image=get_image_in_memory_data(),
-            position=0,
-            content_type=ContentType.objects.get_for_model(TestModel),
-            object_id=self.object.id
-        )
-        self.image.save()
-        self.image = self.get_image()
-
-    def tearDown(self):
-        self.image.delete()
-        self.object.delete()
-        self.second_object.delete()
-        self.another_object.delete()
-
-    @staticmethod
-    def get_image():
-        return models.Image.objects.all()[0]
-
-    @staticmethod
-    def get_name(name):
-        return "/".join([settings.GALLERY_PATH, name])
 
 
 class TestUniqueSlugCheck(ImageTestCase):
