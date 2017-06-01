d0b0b66085bd71c3f41e772807891234cd65410e
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-01 18:33:16 +0300

    Added tests for ImageAdminForm validation and constructor

---
+++ tests/test_forms.py
@@ -0,0 +1,123 @@
+import os
+
+from django.test import mock, TestCase
+from django.contrib.contenttypes.models import ContentType
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django import forms as django_forms
+
+from .. import forms
+from .. import models
+from .. import settings
+
+from .models import TestModel
+from .utils import get_image_in_memory_data, create_image_file
+
+class TestImageAdminForm(TestCase):
+
+    image_path = os.path.join(settings.MEDIA_ROOT, 'foo.jpg')
+
+    @classmethod
+    def setUpClass(cls):
+        create_image_file(cls.image_path)
+        cls.ctype = ContentType.objects.get_for_model(TestModel)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.image_path)
+
+    def test_valid_form(self):
+        upload_file = open(self.image_path, 'rb')
+        post_dict = {
+            'position': 0,
+            'content_type': self.ctype.pk,
+            'object_id': 1
+        }
+        file_dict = {
+            'image': SimpleUploadedFile(upload_file.name, upload_file.read())
+        }
+        form = forms.ImageAdminForm(post_dict, file_dict)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form_without_image(self):
+        post_dict = {
+            'position': 0,
+            'content_type': self.ctype.pk,
+            'object_id': 1
+        }
+        form = forms.ImageAdminForm(post_dict)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_form_without_content_type(self):
+        upload_file = open(self.image_path, 'rb')
+        post_dict = {
+            'position': 0,
+            'object_id': 1
+        }
+        file_dict = {
+            'image': SimpleUploadedFile(upload_file.name, upload_file.read())
+        }
+        form = forms.ImageAdminForm(post_dict, file_dict)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_form_without_object_id(self):
+        upload_file = open(self.image_path, 'rb')
+        post_dict = {
+            'position': 0,
+            'content_type': self.ctype.pk
+        }
+        file_dict = {
+            'image': SimpleUploadedFile(upload_file.name, upload_file.read())
+        }
+        form = forms.ImageAdminForm(post_dict, file_dict)
+        self.assertFalse(form.is_valid())
+
+    def test_valid_form_without_position(self):
+        upload_file = open(self.image_path, 'rb')
+        post_dict = {
+            'content_type': self.ctype.pk,
+            'object_id': 1
+        }
+        file_dict = {
+            'image': SimpleUploadedFile(upload_file.name, upload_file.read())
+        }
+        form = forms.ImageAdminForm(post_dict, file_dict)
+        self.assertTrue(form.is_valid())
+
+    def test_create_empty_form(self):
+        form = forms.ImageAdminForm()
+        self.assertIsNone(form.fields['object_id'].widget.model_class)
+        self.assertNotIsInstance(
+            form.fields['content_type'].widget,
+            django_forms.HiddenInput
+        )
+        self.assertNotIsInstance(
+            form.fields['object_id'].widget,
+            django_forms.HiddenInput
+        )
+
+    def test_create_empty_form_popup(self):
+        form = forms.ImageAdminForm(initial={'_popup': True})
+        self.assertIsInstance(
+            form.fields['content_type'].widget,
+            django_forms.HiddenInput
+        )
+        self.assertIsInstance(
+            form.fields['object_id'].widget,
+            django_forms.HiddenInput
+        )
+
+    def test_create_form_with_instance(self):
+        obj = TestModel.objects.create(name='Test object')
+        image = models.Image.objects.create(
+            image=get_image_in_memory_data(),
+            position=0,
+            content_type=ContentType.objects.get_for_model(TestModel),
+            object_id=obj.id
+        )
+        form = forms.ImageAdminForm(instance=image)
+        self.assertEqual(
+            form.fields['object_id'].widget.model_class,
+            TestModel
+        )
+        image.delete()
+        obj.delete()
