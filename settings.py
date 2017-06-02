247d4e7ecd4deafbcbb351152bb7225812ce4053
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-02 21:48:19 +0300

    Added spaces

--- settings.py
+++ settings.py
@@ -4,14 +4,19 @@ from django.core.exceptions import ImproperlyConfigured
 CONF = {
     'image_width': 752,
     'image_height': 608,
+
     'small_image_width': 564,
     'small_image_height': 456,
+
     'thumbnail_width': 94,
     'thumbnail_height': 76,
+
     'preview_width': 376,
     'preview_height': 304,
+
     'small_preview_width': 141,
     'small_preview_height': 114,
+
     'path': 'gallery',
 }
 
