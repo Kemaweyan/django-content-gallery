e4fc0015e10dd54f85e504491e420d89bdc720f2
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-02 23:57:02 +0300

    Added tests for custom settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -16,3 +16,53 @@ class TestSettings(TestCase):
     def test_without_media_url(self):
         with self.assertRaises(ImproperlyConfigured):
             imp.reload(settings)
+
+    @override_settings(CONTENT_GALLERY={'image_width': 1024})
+    def test_image_width(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['image_width'], 1024)
+
+    @override_settings(CONTENT_GALLERY={'image_height': 768})
+    def test_image_height(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['image_height'], 768)
+
+    @override_settings(CONTENT_GALLERY={'small_image_width': 800})
+    def test_small_image_width(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['small_image_width'], 800)
+
+    @override_settings(CONTENT_GALLERY={'small_image_height': 600})
+    def test_small_image_height(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['small_image_height'], 600)
+
+    @override_settings(CONTENT_GALLERY={'preview_width': 400})
+    def test_preview_width(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['preview_width'], 400)
+
+    @override_settings(CONTENT_GALLERY={'preview_height': 300})
+    def test_preview_height(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['preview_height'], 300)
+
+    @override_settings(CONTENT_GALLERY={'small_preview_width': 200})
+    def test_small_preview_width(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['small_preview_width'], 200)
+
+    @override_settings(CONTENT_GALLERY={'small_preview_height': 150})
+    def test_small_preview_height(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['small_preview_height'], 150)
+
+    @override_settings(CONTENT_GALLERY={'thumbnail_width': 120})
+    def test_thumbnail_width(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['thumbnail_width'], 120)
+
+    @override_settings(CONTENT_GALLERY={'thumbnail_height': 80})
+    def test_thumbnail_height(self):
+        imp.reload(settings)
+        self.assertEqual(settings.CONF['thumbnail_height'], 80)
