7c7465e8dd864c0821ec5054196d570773e4ecca
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-05 22:00:37 +0300

    Added comments to urls.py

--- urls.py
+++ urls.py
@@ -5,11 +5,13 @@ from . import views
 app_name = 'gallery'
 
 urlpatterns = [
+    # a URL for getting a list of objets of the model
     url(
         r'^ajax/choices/(?P<pk>\d+)/$',
         views.choices,
         name='choices'
     ),
+    # a URL for getting data of all images related to the object
     url(
         r'^ajax/gallery_data/(?P<app_label>\w+)/'
         '(?P<content_type>\w+)/(?P<object_id>\d+)/$',
