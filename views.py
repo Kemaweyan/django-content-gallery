6c0438b263f6b5f65dcafced7a61da1d2a689b21
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-06 17:13:27 +0300

    Shortcat get_object_or_404 used instead of raising Http404 manually

--- views.py
+++ views.py
@@ -64,49 +64,43 @@ def gallery_data(request, app_label, content_type, object_id):
         "height": settings.CONF['thumbnail_height']
     }
 
-    try:
-        ctype = ContentType.objects.get(
-            app_label=app_label,
-            model=content_type
-        )
-    except:
-        raise Http404
-    else:
-        obj = get_object_or_404(ctype.model_class(), pk=object_id)
-        qs = obj.gallery.order_by('position')
-        max_size = (
-            settings.CONF['small_image_width'],
-            settings.CONF['small_image_height']
-        )
+    ctype = get_object_or_404(
+        ContentType,
+        app_label=app_label,
+        model=content_type
+    )
+    obj = get_object_or_404(ctype.model_class(), pk=object_id)
+    qs = obj.gallery.order_by('position')
+    max_size = (
+        settings.CONF['small_image_width'],
+        settings.CONF['small_image_height']
+    )
 
-        images = []
-        for img in qs:
-            size = (img.image.width, img.image.height)
-            small_width, small_height = utils.calculate_image_size(
-                size,
-                max_size
-            )
-            images.append({
-                "image": img.image_url,
-                "image_size": {
-                    "width": img.image.width,
-                    "height": img.image.height
-                },
-                "small_image_size": {
-                    "width": small_width,
-                    "height": small_height
-                },
-                "small_image": img.small_image_url,
-                "thumbnail": img.thumbnail_url
-            })
-
-        response = {
-            "images": images,
-            "image_size": image_size,
-            "small_image_size": small_image_size,
-            "thumbnail_size": thumbnail_size,
-        }
-        return HttpResponse(
-            json.dumps(response),
-            content_type='application/json'
+    images = []
+    for img in qs:
+        size = (img.image.width, img.image.height)
+        small_width, small_height = utils.calculate_image_size(
+            size,
+            max_size
         )
+        images.append({
+            "image": img.image_url,
+            "image_size": {
+                "width": img.image.width,
+                "height": img.image.height
+            },
+            "small_image_size": {
+                "width": small_width,
+                "height": small_height
+            },
+            "small_image": img.small_image_url,
+            "thumbnail": img.thumbnail_url
+        })
+
+    response = {
+        "images": images,
+        "image_size": image_size,
+        "small_image_size": small_image_size,
+        "thumbnail_size": thumbnail_size,
+    }
+    return HttpResponse(json.dumps(response), content_type='application/json')
