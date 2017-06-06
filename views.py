81667c13935a121d45a2aa5f0f4c2cc8ac6c6783
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-06 17:53:41 +0300

    Added comments to gallery_data view

--- views.py
+++ views.py
@@ -63,26 +63,44 @@ def gallery_data(request, app_label, content_type, object_id):
         "width": settings.CONF['thumbnail_width'],
         "height": settings.CONF['thumbnail_height']
     }
-
+    # get the ContentType object or raise 404
     ctype = get_object_or_404(
         ContentType,
         app_label=app_label,
         model=content_type
     )
+    # get the object of the model or raise 404
     obj = get_object_or_404(ctype.model_class(), pk=object_id)
-    qs = obj.gallery.order_by('position')
+    qs = obj.gallery.order_by('position')  # order images by 'position'
+    # the target size of the small image from the settings
     max_size = (
         settings.CONF['small_image_width'],
         settings.CONF['small_image_height']
     )
 
+    # Since there is the resizing effect when user switches to another
+    # image, the JavaScript code requires actual sizes of images. But
+    # images could have any aspect ratio so their width or haight could
+    # differ from specified in the settings. Actually settings specify
+    # just maximum values of width and height. But JavaSctipt code needs
+    # real size of each image to keep correct aspect ratio while the effect
+    # is performing.
+    # There is no problem to determine the size of the full-size image
+    # using properties of the ImageField. But with small images we must
+    # calculate the size again using the size of full-size image to determine
+    # the aspect ratio. 
+    # For this purpose we use the 'utils.calculate_image_size' function.
+
     images = []
     for img in qs:
+        # actual size of the full-size image
         size = (img.image.width, img.image.height)
+        # calculate the actual size of the small image
         small_width, small_height = utils.calculate_image_size(
             size,
             max_size
         )
+        # add the data of the image to the list
         images.append({
             "image": img.image_url,
             "image_size": {
@@ -97,10 +115,14 @@ def gallery_data(request, app_label, content_type, object_id):
             "thumbnail": img.thumbnail_url
         })
 
+    # create the response
+    # the size settings are used by JavaScript code to create
+    # HTML containers for images
     response = {
         "images": images,
         "image_size": image_size,
         "small_image_size": small_image_size,
         "thumbnail_size": thumbnail_size,
     }
+    # send the response in JSON format
     return HttpResponse(json.dumps(response), content_type='application/json')
