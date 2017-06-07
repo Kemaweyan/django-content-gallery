280dfb7d681c557bb80a6a32c52459b9571b4a95
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-07 21:44:55 +0300

    Added comments to test/models.py

--- tests/models.py
+++ tests/models.py
@@ -3,9 +3,17 @@ from django.db import models as django_models
 from .. import models
 
 class BaseTestModel(django_models.Model):
+    """
+    An abstract base test model class for test objects that
+    could be related to Images. Contains just one text field
+    """
     name = django_models.CharField(max_length=100)
 
     def __str__(self):
+        """
+        A str version of the object just returns
+        the value of its text field
+        """
         return self.name
 
     class Meta:
@@ -13,12 +21,25 @@ class BaseTestModel(django_models.Model):
 
 
 class TestModel(models.ContentGalleryMixin, BaseTestModel):
-    pass
+    """
+    A main test model. Uses the ContentGalleryMixin without
+    changes, so it allows to show the model in the list of
+    available models in the Image admin.
+    """
 
 
 class AnotherTestModel(models.ContentGalleryMixin, BaseTestModel):
+    """
+    Another test model. It also uses the ContentGalleryMixin, but
+    sets the 'gallery_visible' to False, so it's still possible
+    to attach images to objects of this model, but the model
+    does not present in the available models list in the Image admin.
+    """
     gallery_visible = False
 
 
 class WrongTestModel(BaseTestModel):
-    pass
+    """
+    A test model that does not uses the ContentGalleryMixin.
+    It could not be related to the Image objects.
+    """
