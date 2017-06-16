e76c0251a3c5d3ff1d2e217e13e9f252480bd582
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-16 22:12:13 +0300

    Added testapp

---
+++ runtests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'content_gallery_testapp.settings'
+
+import django
+from django.test.utils import get_runner
+from django.conf import settings
+
+
+def runtests():
+    django.setup()
+    TestRunner = get_runner(settings)
+    runner = TestRunner(verbosity=1, interactive=True)
+    failures = runner.run_tests(['content_gallery'])
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    runtests()
