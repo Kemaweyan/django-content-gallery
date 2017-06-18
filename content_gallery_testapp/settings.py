a0d6532c7afd4e97ba147290ad0c3d43a6fa5bab
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-18 22:56:37 +0300

    Added admin_jqueryui app

--- content_gallery_testapp/settings.py
+++ content_gallery_testapp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'testapp',
-    'content_gallery'
+    'content_gallery',
+    'admin_jqueryui',
 ]
 
 MIDDLEWARE = [
