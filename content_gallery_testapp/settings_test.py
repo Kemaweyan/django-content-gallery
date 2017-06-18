ca7f0e2c915e0b5f6551e1c2a4f6cde1d5c97cd4
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-18 23:12:49 +0300

    Added content_gallery_testapp to PYTHONPATH

--- content_gallery_testapp/settings_test.py
+++ content_gallery_testapp/settings_test.py
@@ -1,7 +1,5 @@
 from .settings import *
 
-ROOT_URLCONF = 'content_gallery_testapp.urls'
-
 INSTALLED_APPS += [
     'content_gallery.tests'
 ]
--- runtests.py
+++ runtests.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+# create a path to the content_gallery_testapp
+base_dir = os.path.dirname(os.path.abspath(__file__)) 
+path = os.path.join(base_dir, 'content_gallery_testapp')
+# insert the path right after current directory
+sys.path.insert(1, path)
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'content_gallery_testapp.settings_test'
 
 import django
