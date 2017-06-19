1bba5d98b8f4dafe5c96fc0d6e90126f3cd0de44
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-19 20:41:16 +0300

    Added media url pattern

--- content_gallery_testapp/urls.py
+++ content_gallery_testapp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     url(r'^testapp/', include('testapp.urls')),
     url(r'^$', RedirectView.as_view(pattern_name="testapp:cat_list")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
