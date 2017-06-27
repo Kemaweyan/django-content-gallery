28fd2a3ce2602a8fb46f8962c2828a53f4708c2f
Gaidukov Taras <kemaweyan@gmail.com>
2017-06-27 18:23:30 +0300

    Added namespace to url include

--- content_gallery_testapp/urls.py
+++ content_gallery_testapp/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^gallery/', include('content_gallery.urls')),
+    url(r'^gallery/', include('content_gallery.urls', namespace='content_gallery')),
     url(r'^testapp/', include('testapp.urls')),
     url(r'^$', RedirectView.as_view(pattern_name="testapp:cat_list")),
 ]
